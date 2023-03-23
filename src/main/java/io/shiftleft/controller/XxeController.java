1fbbf0f373259f2b769244926b0c0f426624d5f9
haby0 <Ha_yub@163.com>
2023-03-23 16:05:56 +0800

    Update XxeController.java

--- src/main/java/io/shiftleft/controller/XxeController.java
+++ src/main/java/io/shiftleft/controller/XxeController.java
@@ -1,41 +1,24 @@
 package io.shiftleft.controller;
 
-import io.shiftleft.data.DataLoader;
-import io.shiftleft.model.Account;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import io.shiftleft.repository.AccountRepository;
-import org.dom4j.io.SAXReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.dom4j.io.SAXReader;
 import org.xml.sax.InputSource;
+import java.io.*;
 
-/**
- * Admin checks login
- */
 
 @RestController
-public class AccountController {
-    @Autowired
-    private AccountRepository accountRepository;
-
-    private static Logger log = LoggerFactory.getLogger(DataLoader.class);
+public class XxeController {
     
-    @RequestMapping(value = "/SAXBuilder/vuln", method = RequestMethod.POST)
-    public String SAXBuilderVuln(HttpServletRequest request) {
+    @RequestMapping(value="/xmlReader/sec1", method = RequestMethod.GET)
+    public String xmlReaderSec1(@RequestParam String data) {
         try {
-            String body = WebUtils.getRequestBody(request);
-            logger.info(body);
-
-            SAXBuilder builder = new SAXBuilder();
-            builder.build(new InputSource(new StringReader(body)));
-            return "SAXBuilder xxe vuln code";
+            SAXReader saxReader = safeXml.getSAXReader();
+            saxReader.read(new InputSource(new StringReader(data)));
         } catch (Exception e) {
-            logger.error(e.toString());
             return EXCEPT;
         }
+
+        return "xmlReader xxe security code";
     }
 
 }
