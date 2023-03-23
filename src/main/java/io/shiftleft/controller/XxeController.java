d79a67d6dcb334b83f471c6f9424341448a52053
haby0 <Ha_yub@163.com>
2023-03-23 16:19:15 +0800

    Update XxeController.java

--- src/main/java/io/shiftleft/controller/XxeController.java
+++ src/main/java/io/shiftleft/controller/XxeController.java
@@ -13,7 +13,7 @@ public class XxeController {
     public String xmlReaderSec1(@RequestParam String data) {
         try {
             SAXReader saxReader = new SAXReader();
-            saxReader.read(new InputSource(new StringReader("aaaaa")));
+            saxReader.read(new InputSource(new StringReader(data)));
         } catch (Exception e) {
             return null;
         }
