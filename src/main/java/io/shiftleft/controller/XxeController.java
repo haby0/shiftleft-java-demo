0a52e946e2c9128438d225426ed221bc7aed07e8
haby0 <Ha_yub@163.com>
2023-03-23 15:58:55 +0800

    Create XxeController.java

---
+++ src/main/java/io/shiftleft/controller/XxeController.java
@@ -0,0 +1,41 @@
+package io.shiftleft.controller;
+
+import io.shiftleft.data.DataLoader;
+import io.shiftleft.model.Account;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import io.shiftleft.repository.AccountRepository;
+import org.dom4j.io.SAXReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.xml.sax.InputSource;
+
+/**
+ * Admin checks login
+ */
+
+@RestController
+public class AccountController {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    private static Logger log = LoggerFactory.getLogger(DataLoader.class);
+    
+    @RequestMapping(value = "/SAXBuilder/vuln", method = RequestMethod.POST)
+    public String SAXBuilderVuln(HttpServletRequest request) {
+        try {
+            String body = WebUtils.getRequestBody(request);
+            logger.info(body);
+
+            SAXBuilder builder = new SAXBuilder();
+            builder.build(new InputSource(new StringReader(body)));
+            return "SAXBuilder xxe vuln code";
+        } catch (Exception e) {
+            logger.error(e.toString());
+            return EXCEPT;
+        }
+    }
+
+}
