f4538379ee9df3f74b504cd257ff8ccc6e6bdbb7
haby0 <Ha_yub@163.com>
2023-03-27 16:54:32 +0800

    Create JacksonController.java

---
+++ src/main/java/io/shiftleft/controller/JacksonController.java
@@ -0,0 +1,23 @@
+package io.shiftleft.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class JacksonController {
+
+    @RequestMapping(value="/jackson/sec1", method = RequestMethod.GET)
+    public String JacksonSec1(@RequestParam String data) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enableDefaultTyping();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        Object obj = mapper.readValue(data, Object.class);
+        return mapper.writeValueAsString(obj);
+    }
+}
